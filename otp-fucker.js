5361f3a6220db37b2eb72ca49b63340ca0fe7edd
Haarolean <gpg@haarolean.dev>
2020-09-14 11:45:18 +0300

    Initial commit

---
+++ otp-fucker.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         OTP fucker
+// @namespace    http://fucktheotpbpc
+// @version      0.1
+// @description  Fucks the two factor authentication in BPC!
+// @author       You
+// @match        https://rbs-develop.paymentgate.ru/*/plugins/servlet/twofactor/public/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/jiangts/JS-OTP/master/dist/jsOTP.min.js
+// @require      https://code.jquery.com/jquery-3.4.1.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var baseurl='https://rbs-develop.paymentgate.ru/';
+    var secrets = [{location: 'bugs/', secret: '{Jira secret}'}, {location: 'wiki/', secret: '{Wiki secret}'}];
+    var secretToUse = '';
+    secrets.forEach(function(secretInfo) {
+        console.log('checking for ' + secretInfo.location);
+        if(location.href.startsWith(baseurl+secretInfo.location)) {
+            secretToUse = secretInfo.secret;
+        }
+    });
+
+
+    var totp = new jsOTP.totp();
+    var otpCode = totp.getOtp(secretToUse);
+    $('#2fpin').val(otpCode);
+    $('#d').submit();
+;})();
